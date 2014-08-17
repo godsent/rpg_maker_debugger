b310393926b9380fe2efd194e968060e2549c708
Sidoruk Nikolay <godsent@tut.by>
2014-08-17 21:59:17 +0300

    tab removed

--- scene_base_patch.rb
+++ scene_base_patch.rb
@@ -1,8 +1,8 @@
 class Scene_Base
   alias_method :original_update, :update
 
-  def update(*args, &block) 
-  	Debugger.load_console if Input.trigger? Debugger::TRIGGER 
+  def update(*args, &block)
+    Debugger.load_console if Input.trigger? Debugger::TRIGGER
     original_update *args, &block
   end
 end
